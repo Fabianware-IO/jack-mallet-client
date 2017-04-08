23f0d7d1f3c75cb4a21458e5229d030b059fdcea
Bobby Smith <bobby.smith@gmail.com>
2017-04-07 19:49:55 -0500

    add deploy assets

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+SHA1=$1
+
+EB_BUCKET=elasticbeanstalk-us-east-1-jackmallet-client
+EB_ENVIRONMENT_NAME="JackMallet-Client-Production"
+AWS_REGION="us-east-1"
+
+DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+
+# Deploy image to dockerhub
+docker push fabianwareio/jackmallet-client:$SHA1
+
+# Create new Elastic Beanstalk version
+sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+aws --region=$AWS_REGION s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+aws --region=$AWS_REGION elasticbeanstalk create-application-version --application-name jackmallet-client \
+	--version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+
+# Update Elastic Beanstalk environment to new version
+aws --region=$AWS_REGION elasticbeanstalk update-environment --environment-name $EB_ENVIRONMENT_NAME \
+	--version-label $SHA1
